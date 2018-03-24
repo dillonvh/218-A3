b4af4fbddd46316e29a949b6fa0224ac88e75d68
vberezny <v.bereznyakov@gmail.com>
2018-03-24 00:25:11 -0700

    changed port#

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 26568;
 
 const options = {
   dotfiles: 'ignore',
